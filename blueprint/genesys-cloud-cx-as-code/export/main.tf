f2488188b4d9ec1efbacff7778836e75f56c7e5d
Bawake <i740483@dsglobal.org>
2026-02-08 17:41:39 -0500

    test

--- blueprint/genesys-cloud-cx-as-code/export/main.tf
+++ blueprint/genesys-cloud-cx-as-code/export/main.tf
@@ -11,15 +11,14 @@ provider "genesyscloud" {
   sdk_debug = true
 }
 
-resource "genesyscloud_tf_export" "queue_export" {
-  directory               = "../modules/exported_queues"
-  export_format           = "hcl"
-  log_permission_errors   = true
-  include_state_file      = false
-  enable_dependency_resolution = false
+resource "genesyscloud_tf_export" "bfsi_poc_flow_export" {
+  directory                          = "./exported_resources"
+  export_format                      = "hcl"
+  log_permission_errors              = true
+  include_state_file                 = false
+  enable_dependency_resolution       = true  # Automatically export all dependencies
+  use_legacy_architect_flow_exporter = false # Export flows in YAML format
   include_filter_resources = [
-    "genesyscloud_routing_queue::^Customer Support$",
-    "genesyscloud_routing_queue::^CUSTOMER SERVICE$",
-    "genesyscloud_routing_queue::^RK$"
+    "genesyscloud_flow::BFSI_POC"  # Export BFSI_POC flow and its dependencies
   ]
 }
